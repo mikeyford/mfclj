659f2e86a541a2c270788075ee753a0198d6bd28
Michael Ford <8105141+mikeyford@users.noreply.github.com>
2018-07-21 14:57:58 +0100

    update symbol names

--- src/mfclj/core.clj
+++ src/mfclj/core.clj
@@ -1,15 +1,15 @@
 (ns mfclj.core
   (:gen-class))
 
-(defn num-map [map]
+(defn num-map [m]
   "Returns input map where values are numbers"
-  (into {} (filter (fn [x] (number? (second x))) map)))
+  (into {} (filter #(number? (second %))) m))
 
 
 (defmacro get-env []
   "Returns map of local bindings, with symbols as keywords"
-  (let [bindings (for [k (keys &env)]
-                   [(name k) k])]
-    (zipmap (map #(keyword (first %)) bindings)
-            (map #(second %) bindings))))
+  (let [b (for [k (keys &env)]
+            [(name k) k])]
+    (zipmap (map #(keyword (first %)) b)
+            (map #(second %) b))))
 
