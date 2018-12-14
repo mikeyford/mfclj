47c08a4b84ee802c124ab871bcf4b7be26a80e88
mikeyford <mfogeo@gmail.com>
2018-12-14 20:40:26 +0100

    WIP decomposition

--- src/mfclj/time.clj
+++ src/mfclj/time.clj
@@ -25,3 +25,27 @@
   (assert (= (reduce + weights) 1))
   (let [window (count weights)]
     (map #(window-average ts window weights %) (range (count ts)))))
+
+
+(defn nil-subtract [a b]
+  (if (or (= a nil) (= b nil))
+    nil
+    (- a b)))
+
+(defn period-avg [detrend mod-map period]
+  (let [indexes (map first (filter #(= period (val %)) mod-map))
+        vals-with-nil (map #(nth detrend %) indexes)
+        vals (filter #(not (= nil %)) vals-with-nil)]
+    {period (/ (reduce + vals) (count vals))}))
+
+(defn decomposition [ts period]
+  "TODO take ags and remap to pattern over full ts
+  Calculate remainder component
+  Return all ts, trend cycle, seasonal and remainder in map."
+  (let [trend-cycle (sma ts period)
+        detrend (map nil-subtract ts trend-cycle)
+        index (range (count ts))
+        mods (zipmap index (map #(mod % period) index))
+        avgs (reduce conj (map #(period-avg detrend mods %) (range period)))]
+    avgs)
+  )
