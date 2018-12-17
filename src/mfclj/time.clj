db5a69196a5f402364630b48da5f0f9ea78742dd
mikeyford <mfogeo@gmail.com>
2018-12-17 16:03:10 +0000

    WIP

--- src/mfclj/time.clj
+++ src/mfclj/time.clj
@@ -27,25 +27,37 @@
     (map #(window-average ts window weights %) (range (count ts)))))
 
 
-(defn nil-subtract [a b]
+(defn nil-operate [a b operator]
   (if (or (= a nil) (= b nil))
     nil
-    (- a b)))
+    (operator a b)))
 
 (defn period-avg [detrend mod-map period]
+  "TODO fix error , looks like empty seq being passed in"
   (let [indexes (map first (filter #(= period (val %)) mod-map))
         vals-with-nil (map #(nth detrend %) indexes)
         vals (filter #(not (= nil %)) vals-with-nil)]
     {period (/ (reduce + vals) (count vals))}))
+
+(defn remap-avgs [avgs period length]
+  (let [base (->> (repeatedly #(range period))
+                  (flatten)
+                  (take length))]
+    (map avgs base)))
+
 
 (defn decomposition [ts period]
   "TODO take ags and remap to pattern over full ts
   Calculate remainder component
   Return all ts, trend cycle, seasonal and remainder in map."
   (let [trend-cycle (sma ts period)
-        detrend (map nil-subtract ts trend-cycle)
+        detrend (map nil-operate ts trend-cycle -)
         index (range (count ts))
         mods (zipmap index (map #(mod % period) index))
-        avgs (reduce conj (map #(period-avg detrend mods %) (range period)))]
-    avgs)
-  )
+        avgs (reduce conj (map #(period-avg detrend mods %) (range period)))
+        seasonal (remap-avgs avgs period (count ts))
+        remainder (map nil-operate detrend seasonal -)]
+    {:ts ts
+     :trend trend-cycle
+     :seasonal seasonal
+     :remainder remainder}))
